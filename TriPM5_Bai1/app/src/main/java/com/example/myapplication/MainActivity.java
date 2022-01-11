08cdde4b94ad48284babc1496b437b213caa4d89
Kuneksa <59677105+tripham208@users.noreply.github.com>
2022-01-11 23:17:10 +0700

    create button

--- TriPM5_Bai1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ TriPM5_Bai1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"xin chào",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
